bec03bdb0a3a796d53d9540fbab57151b0e9790b
portalsoup <portalsoup@gmail.com>
2023-09-27 18:28:27 -0700

    Implemented screen capture

--- api/src/main/kotlin/com/portalsoup/wireprotocol/api/ScreenCapture.kt
+++ api/src/main/kotlin/com/portalsoup/wireprotocol/api/ScreenCapture.kt
@@ -1,5 +1,9 @@
 package com.portalsoup.wireprotocol.api
 
+import com.portalsoup.wireprotocol.dto.Element
+import com.portalsoup.wireprotocol.dto.Session
+import com.portalsoup.wireprotocol.dto.SuccessResponse
 
-fun WireProtocol.takeScreenshot() = Unit
-fun WireProtocol.takeElementScreenshot() = Unit
+
+fun WireProtocol.takeScreenshot(session: Session): SuccessResponse<String> = requestBuilder.get("/session/${session.id}/screenshot")
+fun WireProtocol.takeElementScreenshot(session: Session, element: Element): SuccessResponse<String> = requestBuilder.get("/session/${session.id}/element/${element.reference}/screenshot")
---
+++ api/src/test/kotlin/com/portalsoup/wireprotocol/api/ScreenCaptureTests.kt
@@ -0,0 +1,43 @@
+package com.portalsoup.wireprotocol.api
+
+import com.portalsoup.wireprotocol.BaseTest
+import com.portalsoup.wireprotocol.HtmlPages
+import com.portalsoup.wireprotocol.core.LocationStrategy
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+
+/*
+    These tests require human intervention to verify that the jpgs that are created in your home folder are indeed there and
+    appear correct.
+ */
+class ScreenCaptureTests: BaseTest() {
+
+    @Test
+    fun takeScreenshotTest() {
+        val testPage = HtmlPages.ClickCounter.asUrl()
+        val api = getApi()
+        useSession(api) {
+            api.navigateTo(it, testPage)
+            val base64 = api.takeScreenshot(it).value
+            val decodedImg: ByteArray = Base64.getDecoder().decode(base64.toByteArray())
+            val destinationFile = Paths.get(System.getProperty("user.home"), "testScreenshot.jpg")
+            Files.write(destinationFile, decodedImg)
+        }
+    }
+
+    @Test
+    fun takeElementScreenshotTest() {
+        val testPage = HtmlPages.ClickCounter.asUrl()
+        val api = getApi()
+        useSession(api) {
+            api.navigateTo(it, testPage)
+            val element = api.findElement(it, LocationStrategy.CSS, "#message").value
+            val base64 = api.takeElementScreenshot(it, element).value
+            val decodedImg: ByteArray = Base64.getDecoder().decode(base64.toByteArray())
+            val destinationFile = Paths.get(System.getProperty("user.home"), "testElementScreenshot.jpg")
+            Files.write(destinationFile, decodedImg)
+        }
+    }
+}
