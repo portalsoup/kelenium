821f78f55a1c30914a3bf8fb5b67540b0caa94b7
portalsoup <portalsoup@gmail.com>
2023-09-27 20:41:55 -0700

    Removed unused method

--- api/src/main/kotlin/com/portalsoup/wireprotocol/core/LocationStrategy.kt
+++ api/src/main/kotlin/com/portalsoup/wireprotocol/core/LocationStrategy.kt
@@ -8,11 +8,5 @@ enum class LocationStrategy(val id: String) {
     LINK_TEXT("link text"),
     PARTIAL_TEXT("partial link text"),
     TAG("tag name"),
-    XPATH("xpath");
-
-    companion object {
-        fun match(id: String): LocationStrategy {
-            return entries.find { it.id == id } ?: throw RuntimeException("No Location strategy found for $id")
-        }
-    }
+    XPATH("xpath")
 }
