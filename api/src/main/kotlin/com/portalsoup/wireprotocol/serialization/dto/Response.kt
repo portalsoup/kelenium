db1524d96758ff8da18f66754e4f3dd1e49abd26
portalsoup <portalsoup@gmail.com>
2023-10-03 14:17:38 -0700

    Delete some imports

--- api/src/main/kotlin/com/portalsoup/wireprotocol/serialization/dto/Response.kt
+++ api/src/main/kotlin/com/portalsoup/wireprotocol/serialization/dto/Response.kt
@@ -1,12 +1,7 @@
 package com.portalsoup.wireprotocol.serialization.dto
 
 import com.portalsoup.wireprotocol.serialization.ResponseSerializer
-import com.portalsoup.wireprotocol.serialization.dto.failure.BaseFailure
-import com.portalsoup.wireprotocol.serialization.dto.failure.SessionNotCreated
-import com.portalsoup.wireprotocol.serialization.dto.success.SessionCreated
-import com.portalsoup.wireprotocol.serialization.responseJson
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
 
 @Serializable(with = ResponseSerializer::class)
 data class Response(val value: Any?)
